b8e5ef0b5e39d5f3835a12777dc67718a637a9b0
Liquidibrium <vanoganjelashvili@gmail.com>
2021-05-23 11:20:05 +0400

    add NFA | union

--- Regular-Expresions/build.py
+++ Regular-Expresions/build.py
@@ -1,41 +1,94 @@
-from os import lstat
 from typing import List
-
+from copy import deepcopy
 
 KLEENE_OPERATOR = "*"  # precedence 1
-CONCATENATION = "."  # precedence 2
+CONCATENATION = "+"  # precedence 2
 UNION_EXPRESSION = "|"  # precedence 3
 EMPTY_STRING = "()"
 OPENING_BRACKETS = "("
 CLOSING_BRACKETS = ")"
 LANGUAGE_ALPHABET = "abcd".isalpha()
 
-precedence_dict = {KLEENE_OPERATOR: 1, CONCATENATION: 2, UNION_EXPRESSION: 3}
+precedence_dict = {KLEENE_OPERATOR: 0, CONCATENATION: 1, UNION_EXPRESSION: 2}
 
 
-# graph = {}
+class NFA:
+    def __init__(self, regex) -> None:
+        self.regex = regex
+        self.accepting_states = set()
+        self.states = dict()  # { state:[(symbol,state),] }
+        self.num_states = 0
+        self.index = 0
+
+    def construct_symbol_nfa(self):
+        self.states[self.index] = [(self.regex, self.index + 1)]
+        self.index += 1
+        self.states[self.index] = []
+        self.accepting_states.add(self.index)
+        self.num_states = 2
+
+    def construct_empty_nfa(self):
+        self.accepting_states.add(self.index)
+        self.num_states = 1
+
+    def __or__(self, other):  # union
+        new_accepting_states = set()
+        new_states = dict()
+
+        new_num_states = self.num_states + 1
+
+        start_state_edges = []
+
+        start_state_edges.extend(
+            [  # symbol , state + self.number of state +1
+                (edge[0], edge[1] + 1) for edge in self.states[0]
+            ]
+        )
+        start_state_edges.extend(
+            [  # symbol , state + self.number of state +1
+                (edge[0], edge[1] + new_num_states) for edge in other.states[0]
+            ]
+        )
+
+        new_states[0] = start_state_edges
+
+        for self_accepting_state in self.accepting_states:
+            new_accepting_states.add(self_accepting_state + 1)
+        for other_accepting_state in other.accepting_states:
+            new_accepting_states.add(other_accepting_state + new_num_states)
+
+        for state, edges in self.states.items():
+            new_states[state + 1] = [(edge[0], edge[1] + 1) for edge in edges]
+
+        for state, edges in other.states.items():
+            new_states[state + new_num_states] = [
+                (edge[0], edge[1] + new_num_states) for edge in edges
+            ]
+        new_num_states += other.num_states
+        new_nfa = NFA("tmp")
+        new_nfa.accepting_states = new_accepting_states
+        new_nfa.states = new_states
+        new_nfa.num_states = new_num_states
+        return new_nfa
+
+    def __mul__(self, other):  # star
+        print("mul")
+        pass
+
+    def __add__(self, other):  # concatenation
+        print("add")
+        pass
+
+    def __str__(self) -> str:
+        return f"{self.states}"
 
 
-# def validate_expression(reg_ex):
-#     pass
+def convert_postfix_notation_to_NFA(postfix_queue: List):
+    NFA_stack = []
 
-
-# def transale_to_NFA(reg_ex):
-#     pass
-
-
-# class RegExParser:
-#     def __init__(self, regex) -> None:
-#         self.regex = regex
-#         self.num_nodes = 0
-#         self.accepting_sates = {}  # {states }
-#         self.NFA = {}  # { state:[(symbol,state),] }
-
-#     def _single_simbol(self, symbol):
-#         pass
-
-#     def _single_simbol(self, symbol):
-#         pass
+    for symbol in postfix_queue:
+        if symbol.isalpha():
+            pass
 
 
 def append_operator_to_stack(
@@ -53,7 +106,7 @@ def append_operator_to_stack(
         result_queue.append(operators_stack.pop())
 
 
-def convert_regex_to_posfix_notation(regex: str) -> List:
+def convert_regex_to_postfix_notation(regex: str) -> List:
     result_queue = []
     operators_stack = []
     next_operator_concat = False
@@ -89,12 +142,20 @@ def convert_regex_to_posfix_notation(regex: str) -> List:
 
 
 if __name__ == "__main__":
-    while True:
-        reg_ex = input("Input Regular Expresion: ")
-        if reg_ex == "":
-            print("Finished")
-            break
-        print(convert_regex_to_posfix_notation(reg_ex))
+    a = NFA("a")
+    b = NFA("b")
+    a.construct_symbol_nfa()
+    b.construct_symbol_nfa()
+    new_nfa = a | b
+    print(str(new_nfa))
+
+    # while True:
+    #     reg_ex = input("Input Regular Expresion: ")
+    #     if reg_ex == "":
+    #         print("Finished")
+    #         break
+    #     postfix_que = convert_regex_to_postfix_notation(reg_ex)
+    #     convert_postfix_notation_to_NFA(postfix_que)
 
 # N A T
 # Ai
