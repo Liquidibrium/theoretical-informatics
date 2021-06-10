01b9f620dbb3b4060a52cee654dc90bcb830f5c1
Liquidibrium <vanoganjelashvili@gmail.com>
2021-06-10 16:04:02 +0400

    update run

--- Regular-Expresions/run.py
+++ Regular-Expresions/run.py
@@ -1,16 +1,16 @@
-def translate_inputted_text():
-    states = dict()
-    symbols = dict()
-    final = 0
+import operator
+from functools import reduce
 
-    first_line = input()
+
+def translate_inputted_text() -> tuple[dict[str, list[(int, int), ...]], int]:
+    symbols: dict[str, list[(int, int), ...]] = dict()
+    first_line: str = input()
     n, a, t = map(int, first_line.split())
-    second_line = input()
+    second_line: str = input()
     accepting_states = list(map(int, second_line.split()))
-    for acc in accepting_states:
-        final |= 1 << acc
-
     assert len(accepting_states) == a
+    accepting_states.insert(0, 0)
+    final: int = reduce(lambda prev, ac: prev | (1 << ac), accepting_states)
     for curr_state in range(0, n):
         next_line = input()
         next_states = next_line.split()
@@ -21,51 +21,44 @@ def translate_inputted_text():
             symbol = next_states[i]
             state = int(next_states[i + 1])
             char_state = symbols.get(symbol, None)
-            if not char_state:
-                symbols[symbol] = 1 << state
+            if char_state:
+                symbols[symbol].append((1 << curr_state, 1 << state))
             else:
-                symbols[symbol] |= 1 << state
-            transition_state = states.get(1 << curr_state, None)
-            if not transition_state:
-                states[1 << curr_state] = [1 << state, {symbol}]
-            else:
-                states[1 << curr_state][0] |= 1 << state
-                if symbol in states[1 << curr_state][1]:
-                    # // TODO
-                    states[1 << curr_state][1].add(symbol)
-                else:
-                    states[1 << curr_state][1].add(symbol)
-    return symbols, states, final
+                symbols[symbol] = [(1 << curr_state, 1 << state)]
+
+    return symbols, final
 
 
 # N A T
 # Ai
 # Ki S Aj
-def get_result(string, symbols, states, final):
+def get_result(string: str, symbols: dict[str, list[(int, int), ...]], final: int) -> str:
     result = ""
-    current_state = 1
+    current_state = 1  # initial state 0 , (1 << 0)
     index = len(string)
     for index, symbol in enumerate(string):
-        transition_state = states.get(current_state, None)
-        print(f'symbol {symbol},index {index} ,curr_state {bin(current_state)[2:]}')   # TODO not found but legal
-        if transition_state:
-            print(f"trans_state {transition_state} & symbol_state {bin(symbols[symbol])[2:]} ")
-            if symbol in transition_state[1]:
-                current_state = transition_state[0] & symbols[symbol]
-                print(f"next state {bin(current_state)[2:]}")
-                if final & current_state:
-                    print("Y")
-                    result += "Y"
-                else:
-                    print("N")
-                    result += "N"
+        edges = symbols.get(symbol)
+        if edges:
+            # ab = filter(lambda edg: edg[0] & current_state, edges)
+            # print(list(ab))
+            # ad = reduce(lambda prev, edg: prev | edg[1], ab)
+            next_state = 0
+            for edge in edges:
+                if edge[0] & current_state:
+                    next_state |= edge[1]
+            current_state = next_state
+            if final & current_state:
+                # print("Y")
+                result += "Y"
             else:
-                break
+                # print("N")
+                result += "N"
         else:
             break
-    print("len ", len(string), index)
+
+    # print("len ", len(string), index)
     result += "N" * (len(string) - index)
-    print(result)
+    return result
 
 
 def get_answer():
@@ -73,11 +66,12 @@ def get_answer():
     if word == "":
         print("Finished")
         return
-    symbols, states, final = translate_inputted_text()
-    print(f"symbols: {symbols}\n states: {states}\n final {bin(final)[2:]}")
-    get_result(word, symbols, states, final)
+    symbols, final = translate_inputted_text()
+    # print(f"symbols: {symbols}\n final {bin(final)[2:]}")
+    return get_result(word, symbols, final)
 
 
 if __name__ == "__main__":
     while True:
-        get_answer()
+        answer = get_answer()
+        print(answer)
